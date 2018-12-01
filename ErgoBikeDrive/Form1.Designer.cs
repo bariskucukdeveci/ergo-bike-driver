a2f79f195c1c1ab9e9eecb9b73ba6683e3fe8270
bariskucukdeveci <bariskucukdeveci@gmail.com>
2018-12-01 08:34:38 +0000

    Ergometric Bike Driver

--- ErgoBikeDrive/Form1.Designer.cs
+++ ErgoBikeDrive/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace ErgoBikeDrive
 {
-    partial class Form1
+    partial class myForm
     {
         /// <summary>
         /// Required designer variable.
@@ -28,12 +28,123 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(myForm));
+            this.lblPower = new System.Windows.Forms.Label();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.txtPower = new System.Windows.Forms.TextBox();
+            this.lblBikeImg = new System.Windows.Forms.Label();
+            this.onOffSwitch = new System.Windows.Forms.Button();
+            this.lblPorts = new System.Windows.Forms.Label();
+            this.comboPorts = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // lblPower
+            // 
+            this.lblPower.AutoSize = true;
+            this.lblPower.Font = new System.Drawing.Font("Cambria", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblPower.Location = new System.Drawing.Point(413, 226);
+            this.lblPower.Name = "lblPower";
+            this.lblPower.Size = new System.Drawing.Size(95, 33);
+            this.lblPower.TabIndex = 0;
+            this.lblPower.Text = "Power";
+            // 
+            // btnSend
+            // 
+            this.btnSend.BackColor = System.Drawing.Color.IndianRed;
+            this.btnSend.Enabled = false;
+            this.btnSend.Font = new System.Drawing.Font("Impact", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSend.ForeColor = System.Drawing.Color.White;
+            this.btnSend.Location = new System.Drawing.Point(415, 295);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(234, 59);
+            this.btnSend.TabIndex = 1;
+            this.btnSend.Text = "SEND";
+            this.btnSend.UseVisualStyleBackColor = false;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // txtPower
+            // 
+            this.txtPower.Font = new System.Drawing.Font("Cambria", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtPower.Location = new System.Drawing.Point(521, 218);
+            this.txtPower.Name = "txtPower";
+            this.txtPower.Size = new System.Drawing.Size(128, 46);
+            this.txtPower.TabIndex = 2;
+            this.txtPower.TextChanged += new System.EventHandler(this.txtPower_TextChanged);
+            // 
+            // lblBikeImg
+            // 
+            this.lblBikeImg.Image = ((System.Drawing.Image)(resources.GetObject("lblBikeImg.Image")));
+            this.lblBikeImg.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.lblBikeImg.Location = new System.Drawing.Point(-5, -7);
+            this.lblBikeImg.Name = "lblBikeImg";
+            this.lblBikeImg.Size = new System.Drawing.Size(747, 466);
+            this.lblBikeImg.TabIndex = 3;
+            // 
+            // onOffSwitch
+            // 
+            this.onOffSwitch.BackColor = System.Drawing.Color.Lime;
+            this.onOffSwitch.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.onOffSwitch.Font = new System.Drawing.Font("Cambria", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.onOffSwitch.Location = new System.Drawing.Point(292, 25);
+            this.onOffSwitch.Name = "onOffSwitch";
+            this.onOffSwitch.Size = new System.Drawing.Size(89, 36);
+            this.onOffSwitch.TabIndex = 4;
+            this.onOffSwitch.Text = "Start";
+            this.onOffSwitch.UseVisualStyleBackColor = false;
+            this.onOffSwitch.Click += new System.EventHandler(this.onOffSwitch_Click);
+            // 
+            // lblPorts
+            // 
+            this.lblPorts.AutoSize = true;
+            this.lblPorts.Font = new System.Drawing.Font("Cambria", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblPorts.Location = new System.Drawing.Point(413, 25);
+            this.lblPorts.Name = "lblPorts";
+            this.lblPorts.Size = new System.Drawing.Size(83, 33);
+            this.lblPorts.TabIndex = 6;
+            this.lblPorts.Text = "Ports";
+            // 
+            // comboPorts
+            // 
+            this.comboPorts.Font = new System.Drawing.Font("Cambria", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.comboPorts.FormattingEnabled = true;
+            this.comboPorts.Location = new System.Drawing.Point(521, 27);
+            this.comboPorts.Name = "comboPorts";
+            this.comboPorts.Size = new System.Drawing.Size(128, 31);
+            this.comboPorts.TabIndex = 7;
+            this.comboPorts.SelectedIndexChanged += new System.EventHandler(this.comboPorts_SelectedIndexChanged);
+            // 
+            // myForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.ClientSize = new System.Drawing.Size(737, 452);
+            this.Controls.Add(this.comboPorts);
+            this.Controls.Add(this.lblPorts);
+            this.Controls.Add(this.onOffSwitch);
+            this.Controls.Add(this.txtPower);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.lblPower);
+            this.Controls.Add(this.lblBikeImg);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "myForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Ergo Bike Driver";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblPower;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.TextBox txtPower;
+        private System.Windows.Forms.Label lblBikeImg;
+        private System.Windows.Forms.Button onOffSwitch;
+        private System.Windows.Forms.Label lblPorts;
+        private System.Windows.Forms.ComboBox comboPorts;
     }
 }
 
--- ErgoBikeDrive/Form1.cs
+++ ErgoBikeDrive/Form1.cs
@@ -10,11 +10,141 @@ using System.Windows.Forms;
 
 namespace ErgoBikeDrive
 {
-    public partial class Form1 : Form
+    public partial class myForm : Form
     {
-        public Form1()
+        private bool onOffState = false;
+        System.IO.Ports.SerialPort myPort = new System.IO.Ports.SerialPort("COM10",4800,System.IO.Ports.Parity.None,8);
+        public myForm()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            comboPorts.Items.Clear();
+            string[] ports = System.IO.Ports.SerialPort.GetPortNames();
+            if (ports != null && ports.Length > 0)
+            {
+                comboPorts.Items.AddRange(ports);
+                comboPorts.SelectedItem = comboPorts.Items[0];
+                myPort.PortName = comboPorts.SelectedItem.ToString();
+                onOffSwitch.Enabled = true;
+                onOffSwitch.BackColor = Color.Lime;
+            }
+            else
+            {
+                MessageBox.Show("No ports recognized");
+                onOffSwitch.Enabled = false;
+                onOffSwitch.BackColor = SystemColors.ControlDark;
+            }
+            lblBikeImg.Enabled = false;
+            if (!onOffState)
+            {
+                txtPower.Enabled = false;
+            }
+        }
+
+        private void txtPower_TextChanged(object sender, EventArgs e)
+        {
+            if (textConvert())
+                btnSend.Enabled = true;
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            if (textConvert())
+            {
+                System.Text.ASCIIEncoding aa = new System.Text.ASCIIEncoding();
+
+                byte[] crMessage = aa.GetBytes(System.Windows.Forms.Keys.Return.ToString());
+                byte[] powerStartMessage = aa.GetBytes("W");
+                string s = txtPower.Text;
+                s = s.PadLeft(3, '0');
+                byte[] powerMessage = aa.GetBytes(s);
+                if (myPort.IsOpen)
+                {
+                    myPort.Write(powerStartMessage, 0, powerStartMessage.Length);
+                    myPort.Write(crMessage, 0, powerMessage.Length);
+                    myPort.Write(crMessage, 0, crMessage.Length);
+                }
+            }
+        }
+        private bool textConvert()
+        {
+            string enteredText = txtPower.Text;
+            int power = 0;
+            try
+            {
+                power = Convert.ToInt32(enteredText);
+                if (power > 250 || power < 0)
+                {
+                    MessageBox.Show("Enter an integer between 0 and 250");
+                    return false;
+                }
+                else
+                    return true;
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Wrong text!!!\nEnter an integer...");
+                return false;
+            }
+
+
+        }
+
+        private void onOffSwitch_Click(object sender, EventArgs e)
+        {
+            System.Text.ASCIIEncoding aa = new System.Text.ASCIIEncoding();
+            byte[] startMessage = aa.GetBytes("s");
+            byte[] crMessage = aa.GetBytes(System.Windows.Forms.Keys.Return.ToString());
+            byte[] stopMessage = aa.GetBytes("F");
+            if (!onOffState)
+            { //Starting the Bike.
+                comboPorts.Enabled = false;
+                this.BackColor = Color.White;
+                lblBikeImg.Enabled = true;
+                onOffSwitch.BackColor = Color.Red;
+                onOffState = true;
+                onOffSwitch.Text = "Stop";
+                txtPower.Enabled = true;
+                if (myPort.IsOpen)
+                {
+                    myPort.Write(startMessage, 0, startMessage.Length);
+                    myPort.Write(crMessage, 0, crMessage.Length);
+                }
+                
+            }
+            else
+            { //Stopping the Bike.
+                comboPorts.Enabled = true;
+                this.BackColor = SystemColors.ControlLight;
+                lblBikeImg.Enabled = false;
+                onOffSwitch.BackColor = Color.Lime;
+                onOffState = false;
+                onOffSwitch.Text = "Start";
+                txtPower.Text = "";
+                txtPower.Enabled = false;
+                
+                byte[] zeroPowerMessage = aa.GetBytes("000");
+                byte[] powerStartMessage = aa.GetBytes("W");
+                if (myPort.IsOpen)
+                {
+                    //Zero power send first
+                    myPort.Write(powerStartMessage, 0, powerStartMessage.Length);
+                    myPort.Write(zeroPowerMessage, 0, zeroPowerMessage.Length);
+                    myPort.Write(crMessage, 0, crMessage.Length);
+                    //Stop message sent
+                    myPort.Write(stopMessage, 0, stopMessage.Length);
+                    myPort.Write(crMessage, 0, crMessage.Length);
+                }
+                
+            }
+        }
+
+        private void comboPorts_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            myPort.PortName = comboPorts.SelectedItem.ToString();
         }
     }
 }
